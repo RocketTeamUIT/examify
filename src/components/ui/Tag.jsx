ae559a47dc951a89baab22282e6a6fd13a45c6cc
Tuấn Akira <anhtuan.hoangdinh@gmail.com>
2022-10-27 12:36:22 +0700

    Complete Tag component

--- src/components/ui/Tag.jsx
+++ src/components/ui/Tag.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
 
-const Tag = () => {
-  return <div>Tag</div>;
+/* Props
+- color: tag color. By default, background-opacity is set to 0.2
+- icon: specify tag icon
+*/
+const COLORS = {
+  magenta: '#c41d7f',
+  red: '#cf1322',
+  volcano: '#d4380d',
+  orange: '#d46b08',
+  gold: '#d48806',
+  lime: '#7cb305',
+  green: '#389e0d',
+  cyan: '#08979c',
+  blue: '#096dd9',
+  geek: '1d39c4',
+  purple: '#531dab',
+};
+
+const Tag = ({ children, color, icon }) => {
+  // Check validity of color
+  const checkColor = COLORS[color] === undefined ? color : COLORS[color];
+
+  return (
+    <div
+      className="px-2 py-0.5 rounded-full flex items-center justify-center relative overflow-hidden tag-overlay"
+      style={{
+        '--bg': checkColor,
+      }}
+    >
+      <span
+        className="text-sm font-bold flex items-center justify-center gap-1"
+        style={{
+          color: checkColor,
+        }}
+      >
+        {icon}
+        {children}
+      </span>
+    </div>
+  );
 };
 
 export default Tag;
--- src/layouts/components/Header.jsx
+++ src/layouts/components/Header.jsx
@@ -1,12 +1,11 @@
-import { Button } from '../../components/ui';
+import { Tag } from '../../components/ui';
 import { BiUser } from 'react-icons/bi';
 
 function Header() {
   return (
     <div className="h-screen flex justify-center items-center">
-      <Button size="normal" type="default" leftIcon={<BiUser />} shape="rectangle" rounded={['0', '6px', '6px', '0']}>
-        Button Text
-      </Button>
+      <p className="font-bold">Hoàng Đình Anh Tuấn</p>
+      <Tag color="gold">Admin</Tag>
     </div>
   );
 }
