4281b1306ca24e42c7280ac36d156ee15d12b483
tuannt02 <20522122@gm.uit.edu.vn>
2022-12-18 14:04:12 +0700

    actionsList component testing

--- src/components/ui/ActionsList.jsx
+++ src/components/ui/ActionsList.jsx
@@ -2,10 +2,10 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 import { HiCheck } from 'react-icons/hi';
 
-// Sau này component phình to sẽ tách ra
 function ActionsList({ actionsList, onSelectItem, onChangeItem, ...props }) {
   const [check, setCheck] = useState(0);
 
+  // Define ActionsList but actual used in ActionItem
   const handleClickItem = (title, index) => {
     setCheck(index);
     onSelectItem(); // Hide Actions list when we click on item
@@ -13,38 +13,44 @@ function ActionsList({ actionsList, onSelectItem, onChangeItem, ...props }) {
   };
 
   return (
-    <div
+    <ul
       className="min-w-[180px] max-w-[240px] py-2 bg-bg_white dark:bg-bg_black overflow-hidden rounded border border-br_light_gray dark:border-br_gray"
       {...props}
     >
-      {actionsList.map((data, index) => (
-        <ActionItem index={index} key={index} check={index === check} onAction={data.func} onClick={handleClickItem}>
-          {data.name}
+      {actionsList.map((actionItem, index) => (
+        <ActionItem
+          index={index}
+          key={index}
+          check={index === check}
+          onAction={actionItem.func}
+          onClick={handleClickItem}
+        >
+          {actionItem.name}
         </ActionItem>
       ))}
-    </div>
+    </ul>
   );
 }
 
-function ActionItem({ children, check, index, onClick, onAction }) {
+export function ActionItem({ children, check, index, onClick, onAction }) {
   const handleOnClick = () => {
     onClick(children, index);
     onAction();
   };
 
   return (
-    <div
+    <li
       onClick={handleOnClick}
       className="flex items-center justify-between px-3 py-2 text-t_dark dark:text-t_light_gray text-h6 font-medium cursor-pointer hover:bg-bg_light_gray dark:hover:bg-bg_dark_gray active:bg-bg_dark_gray active:text-t_light_gray dark:active:bg-bg_light_gray dark:active:text-t_dark"
     >
       <span>{children}</span>
-      <span className="text-t_dark dark:text-t_white">{check && <HiCheck />}</span>
-    </div>
+      {check && (
+        <span title="icon" className="text-t_dark dark:text-t_white">
+          <HiCheck />
+        </span>
+      )}
+    </li>
   );
-}
-
-function Divider() {
-  return <div className="my-2 h-[1px] bg-br_light_gray dark:bg-br_gray"></div>;
 }
 
 ActionItem.defaultProps = {
---
+++ src/components/ui/__test__/ActionsList.test.jsx
@@ -0,0 +1,108 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import ActionsList, { ActionItem } from '../ActionsList';
+
+const THREE_ITEM = [
+  {
+    name: 'Tăng dần',
+    func: () => {
+      console.log('Tăng dần');
+    },
+  },
+  {
+    name: 'Giảm dần',
+    func: () => {
+      console.log('Giảm dần');
+    },
+  },
+  {
+    name: 'Theo chữ cái A - Z',
+    func: () => {
+      console.log('Theo chữ cái A - Z');
+    },
+  },
+];
+const TWO_ITEM = [
+  {
+    name: 'Tăng dần',
+    func: () => {
+      console.log('Tăng dần');
+    },
+  },
+  {
+    name: 'Giảm dần',
+    func: () => {
+      console.log('Giảm dần');
+    },
+  },
+];
+const ONE_ITEM = [
+  {
+    name: 'Tăng dần',
+    func: () => {
+      console.log('Tăng dần');
+    },
+  },
+];
+const ZERO_ITEM = [];
+
+describe('ActionList', () => {
+  test('ActionsList render correctly', () => {
+    render(<ActionsList actionsList={THREE_ITEM} />);
+    const listElement = screen.getByRole('list');
+    expect(listElement).toBeInTheDocument();
+  });
+
+  test('ActionsList render a list item', () => {
+    render(<ActionsList actionsList={THREE_ITEM} />);
+    const listItemElements = screen.getAllByRole('listitem');
+    expect(listItemElements).toHaveLength(THREE_ITEM.length);
+  });
+
+  test('ActionsList render with zero item', () => {
+    render(<ActionsList actionsList={ZERO_ITEM} />);
+    const listElement = screen.getByRole('list');
+    expect(listElement).toContainHTML('');
+  });
+
+  test('ActionsList render with one item', () => {
+    render(<ActionsList actionsList={ONE_ITEM} />);
+    const listElement = screen.getByRole('list');
+    const listItemElements = screen.getAllByRole('listitem');
+    expect(listElement).toContainElement(listItemElements[0]);
+    expect(listItemElements).toHaveLength(ONE_ITEM.length);
+  });
+
+  test('ActionsList render with two item', () => {
+    render(<ActionsList actionsList={TWO_ITEM} />);
+    const listElement = screen.getByRole('list');
+    const listItemElements = screen.getAllByRole('listitem');
+    expect(listElement).toContainElement(listItemElements[0]);
+    expect(listItemElements).toHaveLength(TWO_ITEM.length);
+  });
+});
+
+describe('ActionItem', () => {
+  test('ActionItem render correctly', () => {
+    render(<ActionItem index={0} children={ONE_ITEM[0].name} onAction={ONE_ITEM[0].func} onClick={() => {}} />);
+    const actionItem = screen.getByRole('listitem');
+    expect(actionItem).toBeInTheDocument();
+    expect(actionItem).toHaveTextContent(ONE_ITEM[0].name);
+  });
+
+  test('ActionItem render with check icon', () => {
+    render(
+      <ActionItem index={0} check={false} children={ONE_ITEM[0].name} onAction={ONE_ITEM[0].func} onClick={() => {}} />,
+    );
+    const checkIcon = screen.queryByTitle('icon');
+    expect(checkIcon).not.toBeInTheDocument();
+  });
+
+  test('ActionItem render without check icon', () => {
+    render(
+      <ActionItem index={0} check={true} children={ONE_ITEM[0].name} onAction={ONE_ITEM[0].func} onClick={() => {}} />,
+    );
+    const checkIcon = screen.queryByTitle('icon');
+    expect(checkIcon).toBeInTheDocument();
+  });
+});
