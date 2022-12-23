9532451c76781c979a3cd7fed32c5cb3812c979f
KoraShi <pttu2902@gmail.com>
2022-12-23 17:14:59 +0700

    #10 - add old password field for change password panel

--- src/validations/changePassword.js
+++ src/validations/changePassword.js
@@ -1,6 +1,7 @@
 import * as yup from 'yup';
 
 export const changePasswordScheme = yup.object().shape({
+  oldPassword: yup.string().trim().required('Vui lòng nhập trường này!'),
   password: yup
     .string()
     .trim()
