708d44d32c34fd4ca59f31400d20c954fea83d40
tuannt02 <20522122@gm.uit.edu.vn>
2023-01-10 13:21:06 +0700

    persist app

--- src/store.js
+++ src/store.js
@@ -1,16 +1,36 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import authReducer from './features/auth/authSlice';
 import courseReducer from './features/course/courseSlice';
 import noteReducer from './features/note/noteSlice';
 import ratingReducer from './features/rating/ratingSlice';
 
-const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    course: courseReducer,
-    note: noteReducer,
-    rating: ratingReducer,
-  },
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  course: courseReducer,
+  note: noteReducer,
+  rating: ratingReducer,
 });
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export let persistor = persistStore(store);
 
 export default store;
