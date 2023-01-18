84da75f1e7f41ed52cf5d7369b9e59416bccd295
tuannt02 <20522122@gm.uit.edu.vn>
2023-01-18 16:39:26 +0700

    create Radio component

---
+++ src/components/form/Radio.jsx
@@ -0,0 +1,21 @@
+function Radio({ label, name, leftDockLabel, mb }) {
+  return (
+    <label
+      className="text-h5 group block relative select-none cursor-pointer"
+      style={{
+        paddingLeft: leftDockLabel,
+        marginBottom: mb,
+      }}
+    >
+      {label}
+
+      {/* Hide the browser's default checkbox */}
+      <input type="radio" name={name} className="peer absolute opacity-0 cursor-pointer h-0 w-0" />
+
+      {/* Create custom checkmark */}
+      <span className="group-hover:bg-[#ccc] peer-checked:border-t_dark peer-checked:bg-t_dark rounded-full absolute top-[2px] left-0 w-4 h-4 border border-solid border-t_light_gray_3 bg-transparent"></span>
+    </label>
+  );
+}
+
+export default Radio;
