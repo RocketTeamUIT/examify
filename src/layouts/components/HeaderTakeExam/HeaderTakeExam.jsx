a5c45766e74ab22e19daeb5482ccd7057bffaa16
tuannt02 <20522122@gm.uit.edu.vn>
2023-01-23 10:53:11 +0700

    ConfirmModal for submit exam & close exam

--- src/layouts/components/HeaderTakeExam/HeaderTakeExam.jsx
+++ src/layouts/components/HeaderTakeExam/HeaderTakeExam.jsx
@@ -2,8 +2,20 @@ import { Button } from 'components/ui';
 import { Link } from 'react-router-dom';
 import Logo from '../../../assets/Logo.svg';
 import Avatar from '../Avatar';
+import { ModalConfirm } from 'components/ui/Modal';
+import { useState } from 'react';
 
 function HeaderTakeExam() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const showModal = () => setModalVisible(true);
+  const hideModal = () => setModalVisible(false);
+
+  const handleSubmitExam = () => {
+    console.log('handle submit exam');
+    // Call API at here
+  };
+
+  // Mock data
   const user = {
     email: '20522122@gm.uit.edu.vn',
     firstName: 'Tuấn',
@@ -12,28 +24,36 @@ function HeaderTakeExam() {
   };
 
   return (
-    <div className="h-[60px] border bg-white shadow-sd_primary px-6 flex items-center justify-end sm:justify-between sticky top-0 z-20">
-      {/* Logo */}
-      <Link to="/" className="hidden sm:block">
-        <img src={Logo} alt="Examify Logo - Learn and Practice English" className="h-10" />
-      </Link>
+    <>
+      <div className="h-[60px] border bg-white shadow-sd_primary px-6 flex items-center justify-end sm:justify-between sticky top-0 z-20">
+        {/* Logo */}
+        <Link to="/" className="hidden sm:block">
+          <img src={Logo} alt="Examify Logo - Learn and Practice English" className="h-10" />
+        </Link>
 
-      <div className="flex gap-7 ">
-        <div className="flex gap-4">
-          <div className="border rounded-md text-h5 px-4 py-[11px]">
-            <span>67:11</span>
+        <div className="flex gap-7 ">
+          <div className="flex gap-4">
+            <div className="border rounded-md text-h5 px-4 py-[11px]">
+              <span>67:11</span>
+            </div>
+
+            <Button onClick={modalVisible ? hideModal : showModal}>Nộp bài</Button>
           </div>
 
-          <Button>Nộp bài</Button>
-        </div>
-
-        <div data-testid="user-avatar" className="gap-4 flex">
-          {user?.firstName && (
-            <Avatar avt={user.avt} lastName={user.lastName} firstName={user.firstName} email={user.email} />
-          )}
+          <div data-testid="user-avatar" className="gap-4 flex">
+            {user?.firstName && (
+              <Avatar avt={user.avt} lastName={user.lastName} firstName={user.firstName} email={user.email} />
+            )}
+          </div>
         </div>
       </div>
-    </div>
+      <ModalConfirm
+        message="Thời gian vẫn còn nhiều. Bạn có chắc chắn muốn nộp bài?"
+        isShowing={modalVisible}
+        onHide={hideModal}
+        onResolve={handleSubmitExam}
+      />
+    </>
   );
 }
 
