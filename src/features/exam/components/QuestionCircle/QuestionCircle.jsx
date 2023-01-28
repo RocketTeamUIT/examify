931b0d8993cc77701002b88da3ca701a41567927
tuannt02 <20522122@gm.uit.edu.vn>
2023-01-28 09:06:40 +0700

    Upgrade Radio, Question Circle and sth

--- src/features/exam/components/QuestionCircle/QuestionCircle.jsx
+++ src/features/exam/components/QuestionCircle/QuestionCircle.jsx
@@ -1,29 +1,24 @@
-import classNames from 'classnames';
+import QuestionNumber from './QuestionNumber';
+import { forwardRef } from 'react';
+import { useDispatch } from 'react-redux';
+import { toggleModalVisible } from 'features/exam/recordSlice';
 
-function QuestionCircle({ children: numberOrder, type = 'solid' }) {
+function QuestionCircle({ children: numberOrder, type = 'solid' }, ref) {
+  const dispatch = useDispatch();
+
   return (
-    <button
-      className={classNames('w-8 h-8 bg-white ring-1 flex justify-center items-center rounded-full', {
-        'ring-ac_green': type === 'success',
-        'hover:bg-[#34c63533]': type === 'success',
-        'ring-ac_red': type === 'wrong',
-        'hover:bg-[#EF373733]': type === 'wrong',
-        'ring-bg_light_gray': type === 'solid',
-        'bg-bg_light_gray': type === 'solid',
-      })}
-    >
-      <span
-        className={classNames('font-normal text-sm', {
-          'text-ac_green': type === 'success',
-          'text-ac_red': type === 'wrong',
-          'text-primary': type === 'solid',
-          '!font-bold': type === 'solid',
-        })}
+    <>
+      <QuestionNumber
+        type={type}
+        onClick={() => {
+          dispatch(toggleModalVisible());
+        }}
+        ref={ref}
       >
         {numberOrder}
-      </span>
-    </button>
+      </QuestionNumber>
+    </>
   );
 }
 
-export default QuestionCircle;
+export default forwardRef(QuestionCircle);
